776cd8ed2c4c9c43e7a95b07f4e860c0da96c7ee
Ryanshinta <ryanouyangyunru@gmail.com>
2022-09-29 21:13:51 +0800

    Map function Complete

--- app/src/main/java/com/taruc/foodbank/AdminNewFoodBankActivity.kt
+++ app/src/main/java/com/taruc/foodbank/AdminNewFoodBankActivity.kt
@@ -8,48 +8,131 @@ import android.location.Geocoder
 import android.location.Location
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.os.PersistableBundle
-import android.provider.ContactsContract.Data
 import android.util.Log
+import android.widget.ArrayAdapter
+import android.widget.Button
+import android.widget.CheckBox
 import android.widget.EditText
+import android.widget.LinearLayout
+import android.widget.ListView
 import android.widget.Toast
 import androidx.core.app.ActivityCompat
 import androidx.core.app.ActivityCompat.OnRequestPermissionsResultCallback
 import androidx.core.content.ContextCompat
-import androidx.databinding.DataBindingUtil
-import com.google.android.gms.location.FusedLocationProviderClient
-import com.google.android.gms.location.LocationServices
 import com.google.android.gms.maps.*
 import com.google.android.gms.maps.GoogleMap.OnMyLocationButtonClickListener
 import com.google.android.gms.maps.GoogleMap.OnMyLocationClickListener
 import com.google.android.gms.maps.model.LatLng
-import com.google.android.gms.maps.model.Marker
-import com.google.android.gms.maps.model.MarkerOptions
+import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
 import com.taruc.foodbank.PermissionUtils.PermissionDeniedDialog.Companion.newInstance
 import com.taruc.foodbank.PermissionUtils.isPermissionGranted
-import com.taruc.foodbank.databinding.ActivityAdminNewFoodBankBinding
+import com.taruc.foodbank.entity.foodBank
 import java.io.IOException
+import java.time.LocalDate
 import java.util.*
-import javax.annotation.meta.When
+import kotlin.collections.ArrayList
+import kotlin.collections.HashMap
 
 class AdminNewFoodBankActivity : AppCompatActivity(),
     OnMapReadyCallback ,OnRequestPermissionsResultCallback,
     OnMyLocationButtonClickListener,OnMyLocationClickListener{
     private var permissionDenied = false
     private lateinit var map: GoogleMap
-    //private lateinit var binding: ActivityAdminNewFoodBankBinding
+    private var address:String = ""
+    private var latLng:LatLng = LatLng(0.0,0.0)
+
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_admin_new_food_bank)
         val mapFragment = supportFragmentManager.findFragmentById(R.id.map) as SupportMapFragment?
-        //binding = DataBindingUtil.setContentView(this,R.layout.activity_admin_new_food_bank)
+
         mapFragment?.getMapAsync(this)
+        val db = FirebaseFirestore.getInstance()
+        val checkBox = findViewById<CheckBox>(R.id.checkBox)
+        val etName = findViewById<EditText>(R.id.etFoodBankName)
+        val etDesc = findViewById<EditText>(R.id.etDesciption)
+        val etContact = findViewById<EditText>(R.id.etContect)
+        val etAddress = findViewById<EditText>(R.id.etAddress)
+
+        val llFoodInit = findViewById<LinearLayout>(R.id.llFoodInit)
+        val lvFood = findViewById<ListView>(R.id.lvFood)
+        val btAdd = findViewById<Button>(R.id.btFoodAdd)
+        val etFood = findViewById<EditText>(R.id.etFood)
+        val btCreate = findViewById<Button>(R.id.btNewFoodBank)
+
+        var foodList = ArrayList<String>()
+        llFoodInit.visibility = android.view.View.GONE
+
+        db
+
+
+
+
+        val adapter = ArrayAdapter(this,android.R.layout.simple_list_item_1,foodList)
+        lvFood.adapter = adapter
+
+        checkBox.setOnClickListener(){
+            if (checkBox.isChecked){
+                llFoodInit.visibility = android.view.View.VISIBLE
+                lvFood.visibility = android.view.View.VISIBLE
+            }else{
+                llFoodInit.visibility = android.view.View.GONE
+                lvFood.visibility = android.view.View.GONE
+                foodList.clear()
+            }
+        }
+
+        btAdd.setOnClickListener(){
+
+            var foodName = etFood.text.toString()
+            Log.i("addFood",foodName.toString())
+            foodList.add(foodName)
+            adapter.notifyDataSetChanged()
+        }
+
+        btCreate.setOnClickListener(){
+            var foods:Map<String,Int> = mapOf()
+            for (name in foodList){
+                foods = mapOf(
+                    name to 1
+                )
+            }
+            var foodBankDb = hashMapOf(
+                "address" to etName.text.toString(),
+                "contactNumber" to etContact.text.toString(),
+                "description" to etDesc.text.toString(),
+                "foods" to foods,
+                "lat" to latLng.latitude,
+                "lng" to latLng.longitude,
+                "name" to etName.text.toString()
+            )
+
+
+            db.collection("foodbanks").document(etName.text.toString()).set(foodBankDb)
+                .addOnSuccessListener {
+                    Log.i("FoodBank Create","Added Successfully")
+                }
+                .addOnFailureListener {
+                    Log.e("FoodBank Create", it.toString())
+                }
+
+        }
+
+
 
 
     }
 
     override fun onMapReady(googleMap: GoogleMap) {
         map = googleMap
+        val location = LatLng(3.00,101.55)
+        map.uiSettings.isZoomControlsEnabled = true
+        map.moveCamera(CameraUpdateFactory.newLatLng(location))
+
         googleMap.setOnMyLocationButtonClickListener(this)
         googleMap.setOnMyLocationClickListener(this)
         enableMyLocation()
@@ -147,6 +230,7 @@ class AdminNewFoodBankActivity : AppCompatActivity(),
     override fun onMyLocationClick(location: Location) {
         Toast.makeText(this, "Current location:\n$location", Toast.LENGTH_LONG)
             .show()
+
         var geocoder = Geocoder(applicationContext,Locale.getDefault())
         var addressString:String
 
@@ -170,6 +254,8 @@ class AdminNewFoodBankActivity : AppCompatActivity(),
                 addressString = sb.toString()
                 val etAddress = findViewById<EditText>(R.id.etAddress)
                 etAddress.setText(addressString)
+
+                latLng = LatLng(location.latitude,location.longitude)
             }
         }catch (e:IOException){
             Toast.makeText(applicationContext,"Unable connect to Geocoder",Toast.LENGTH_LONG).show()
